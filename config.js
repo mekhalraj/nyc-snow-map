f43e80ab91934825b1d6096e910bc6bbdccaef11
Mekhal Raj Rajendran <86925481+mekhalraj@users.noreply.github.com>
2026-03-12 00:39:53 -0400

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js — DO NOT COMMIT (listed in .gitignore)
-// Your Mapbox public access token
-const MAPBOX_TOKEN = 'pk.eyJ1IjoibWVraGFscmFqIiwiYSI6ImNtOGVlbTdscjA5ZnAyanBzNXZ0d3dlbXEifQ.KLLOFwx0FJxUFqEqilVo4w';
